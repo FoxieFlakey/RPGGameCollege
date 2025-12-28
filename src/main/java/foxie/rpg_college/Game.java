2cf8637945eb7a3f91c6883d72a270506984e517
Foxie Flakey <foxieflakey@gmail.com>
2025-12-28 15:59:09 +0700

    Switch to AWT for finer grained control

--- src/main/java/foxie/rpg_college/Game.java
+++ src/main/java/foxie/rpg_college/Game.java
@@ -1,88 +1,58 @@
 package foxie.rpg_college;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Window;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.Optional;
 
 import foxie.rpg_college.world.World;
 
-public class Game {
-  private final JFrame window;
-  private final GameView gameView;
-  private boolean isRendering = false;
+public class Game implements AutoCloseable {
+  private boolean isRunning = false;
+  private boolean isClosed = false;
 
-  private final JPanel gamePanel;
-  private final JPanel actionPanel;
+  private final Window window;
+  private final BufferStrategy windowBufferStrategy;
 
   private World currentWorld = new World(this);
+  private final BufferedImage gameBuffer = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_RGB);
   private final Camera camera = new Camera(this.currentWorld.getWorldBound(), new Vec2(1280.0f, 720.0f));
 
   public static final int TICK_RATE = 20;
   public static final int REFRESH_RATE = 30;
 
   public Game() {
-    /**
-     * The hierrarchy of the window is
-     * root
-     *  |
-     *  -> Game panel
-     *     |
-     *     -> Game view (the game's output)
-     *     -> Game control (controls for the game, like attack, defense, etc)
-     */
-
-    this.gameView = new GameView(1280, 720);
-
-    JPanel panel = new JPanel();
-    panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-
-    this.gamePanel = new JPanel();
-    this.actionPanel = new JPanel();
-    
-    this.initGamePanel();
-
-    panel.add(this.gamePanel);
-
-    this.window = new JFrame("Game");
-    this.window.getContentPane().add(panel);
-    this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.window.setResizable(false);
-    this.window.validate();
-    this.window.pack();
+    this.window = new Window(null);
+    this.window.setSize(this.getOutputWidth(), this.getOutputHeight());
     this.window.setVisible(true);
-  }
+    this.window.setFocusable(true);
 
-  void initGamePanel() {
-    this.gamePanel.setLayout(new BoxLayout(this.gamePanel, BoxLayout.Y_AXIS));
-    this.gamePanel.add(this.gameView);
-    
-    Dimension gameViewDimension = new Dimension(this.gameView.getViewWidth(), this.gameView.getViewHeight());
-    this.gameView.setMinimumSize(gameViewDimension);
-    this.gameView.setPreferredSize(gameViewDimension);
-    this.gameView.setMaximumSize(gameViewDimension);
-    
-    this.camera.setPosition(new Vec2(0.0f, 0.0f));
+    @SuppressWarnings("resource")
+    final Game game = this;
 
-    this.actionPanel.setLayout(new FlowLayout());
-    this.actionPanel.setMinimumSize(new Dimension(0, 100));
-    this.actionPanel.setPreferredSize(new Dimension(0, 100));
-
-    JButton button = new JButton("Hello");
-    button.addActionListener(new ActionListener() {
+    this.window.addWindowListener(new WindowAdapter() {
       @Override
-      public void actionPerformed(ActionEvent e) {
-        System.out.println("Clicked");
+      public void windowClosing(WindowEvent e) {
+        game.isClosed = true;
       }
     });
-    this.actionPanel.add(button);
-    
-    this.gamePanel.add(this.actionPanel);
+
+    this.window.createBufferStrategy(2);
+    this.windowBufferStrategy = Optional.ofNullable(this.window.getBufferStrategy()).get();
+  }
+
+  @Override
+  public void close() throws Exception {
+    this.windowBufferStrategy.dispose();
+    this.window.dispose();
+  }
+
+  public boolean isClosed() {
+    return this.isClosed;
   }
 
   public World getCurrentWorld() {
@@ -93,13 +63,32 @@ public class Game {
     return this.camera;
   }
 
-  private boolean positive = true;
-  public void render() {
-    if (this.isRendering) {
-      throw new IllegalStateException("Cannot render inside another render");
-    }
-    this.isRendering = true;
+  public int getOutputHeight() {
+    int ret = this.gameBuffer.getHeight();
+    assert ret > 0;
+    return ret;
+  }
 
+  public int getOutputWidth() {
+    int ret = this.gameBuffer.getWidth();
+    assert ret > 0;
+    return ret;
+  }
+
+  private boolean positive = true;
+  public void runOnce() {
+    if (this.isRunning) {
+      throw new IllegalStateException("Cannot run game inside running game");
+    }
+    this.isRunning = true;
+
+    this.tick();
+    this.render();
+
+    this.isRunning = false;
+  }
+
+  void render() {
     Vec2 newPos = this.camera.getPosition();
     if (positive) {
       newPos = newPos.add(new Vec2(20.0f, 0.0f));
@@ -114,11 +103,27 @@ public class Game {
     }
     this.camera.setPosition(newPos);
 
-    this.gameView.runRenderCode((g) -> {
+    Graphics2D g = this.gameBuffer.createGraphics();
+    try {
       this.currentWorld.render(g);
-    });
+    } finally {
+      g.dispose();
+    }
 
-    this.isRendering = false;
+    do {
+      do {
+        Graphics gr = this.windowBufferStrategy.getDrawGraphics();
+        gr.drawImage(
+          this.gameBuffer,
+          0,
+          0,
+          null
+        );
+        gr.dispose();
+      } while (this.windowBufferStrategy.contentsRestored());
+
+      this.windowBufferStrategy.show();
+    } while (this.windowBufferStrategy.contentsLost());
   }
 
   void tick() {
--- src/main/java/foxie/rpg_college/Main.java
+++ src/main/java/foxie/rpg_college/Main.java
@@ -6,16 +6,19 @@ package foxie.rpg_college;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    Game game = new Game();
+    try (Game game = new Game()) {
+      while (!game.isClosed()) {
+        game.runOnce();
 
-    while (true) {
-      game.render();
-
-      try {
-        Thread.sleep(1000 / 60);
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
+        try {
+          Thread.sleep(1000 / 60);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
       }
+    } catch (Exception e) {
+      System.err.println("Error cleaning up game:");
+      e.printStackTrace();
     }
   }
 }
