ab303055067e96956db63aaefc7e1962cb3b295f
Foxie Flakey <foxieflakey@gmail.com>
2025-12-30 12:55:22 +0700

    Make the class bit clean

--- src/main/java/foxie/rpg_college/entity/LivingEntity.java
+++ src/main/java/foxie/rpg_college/entity/LivingEntity.java
@@ -4,7 +4,23 @@ public abstract class LivingEntity extends Entity {
   private float healthPoint;
 
   public boolean isDead() {
-    return this.healthPoint < 0.0f;
+    return this.healthPoint <= 0.0f;
+  }
+
+  public float getHealth() {
+    if (this.healthPoint <= 0.0f) {
+      return 0.0f;
+    }
+
+    return this.healthPoint;
+  }
+
+  public void setHealth(float health) {
+    this.healthPoint = health;
+    
+    if (this.healthPoint <= 0.0f) {
+      this.healthPoint = -1.0f;
+    }
   }
 
   public void doDamage(float damage) {
@@ -12,11 +28,7 @@ public abstract class LivingEntity extends Entity {
       damage = 0.0f;
     }
 
-    this.healthPoint -= damage;
-
-    if (this.healthPoint < 0.0) {
-      this.healthPoint = -1.0f;
-    }
+    this.setHealth(this.healthPoint - damage);
   }
 }
 
