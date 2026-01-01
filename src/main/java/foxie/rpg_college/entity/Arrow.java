e33315f47d5a81da4f8f7eefc2d58e1eebb7b5dd
Foxie Flakey <foxieflakey@gmail.com>
2026-01-01 18:49:33 +0700

    Split render size and collision size

--- src/main/java/foxie/rpg_college/entity/Arrow.java
+++ src/main/java/foxie/rpg_college/entity/Arrow.java
@@ -17,10 +17,11 @@ import foxie.rpg_college.Vec2;
 import foxie.rpg_college.tile.Tile;
 
 public class Arrow extends Entity {
-  private static final Vec2 SIZE = new Vec2(32.0f, 32.0f);
+  private static final Vec2 RENDER_SIZE = new Vec2(12.0f, 28.0f).mul(2.5f);
+  private static final Vec2 COLLISION_SIZE = new Vec2(5.0f, 5.0f);
   
   private boolean hasArrowHitSomething = false;
-  private CollisionBox collisionBox = new CollisionBox(0.1f, new Vec2(0.0f, 0.0f), Arrow.SIZE);
+  private CollisionBox collisionBox = new CollisionBox(0.1f, new Vec2(0.0f, 0.0f), Arrow.COLLISION_SIZE);
   private float damage = 5.0f;
   
   private final Entity shooter;
@@ -82,13 +83,13 @@ public class Arrow extends Entity {
 
   @Override
   public void render(Graphics2D g, float deltaTime) {
-    FloatRectangle renderBox = EntityHelper.calculateRenderBox(this, Arrow.SIZE);
+    FloatRectangle renderBox = EntityHelper.calculateRenderBox(this, Arrow.RENDER_SIZE);
     AffineTransform transform = EntityHelper.calculateCameraTransform(this);
+    transform.translate(-Arrow.RENDER_SIZE.x() * 0.5f, -Arrow.RENDER_SIZE.x() * 0.5f);
     transform.scale(
       renderBox.getSize().x() / (float) Arrow.ARROW_TEXTURE.getWidth(null),
       renderBox.getSize().y() / (float) Arrow.ARROW_TEXTURE.getHeight(null)
     );
-    transform.translate(-Arrow.SIZE.x() * 0.25f, -Arrow.SIZE.y() * 0.5f);
     
     g.drawImage(Arrow.ARROW_TEXTURE, transform, null);
   }
