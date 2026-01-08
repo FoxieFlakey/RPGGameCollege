16b417d9024716ae02bd78cd3e1913ec2430e7b7
Foxie Flakey <foxieflakey@gmail.com>
2026-01-08 23:01:20 +0700

    Fix collision box

--- src/main/java/foxie/rpg_college/entity/FireballEntity.java
+++ src/main/java/foxie/rpg_college/entity/FireballEntity.java
@@ -23,7 +23,7 @@ public class FireballEntity extends ProjectileEntity {
   private final CollisionBox collisionBox = new CollisionBox(
     100.0f,
     new Vec2(0.0f, 0.0f),
-    new Vec2(150.0f, 150.0f),
+    new Vec2(120.0f, 120.0f),
     false
   );
   
