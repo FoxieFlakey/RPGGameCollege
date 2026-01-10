25663034fceb5fd01aa815d3576821591219c663
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 16:13:08 +0700

    Document Mouse.java

--- src/main/java/foxie/rpg_college/input/Mouse.java
+++ src/main/java/foxie/rpg_college/input/Mouse.java
@@ -8,6 +8,8 @@ import java.awt.event.MouseEvent;
 import foxie.rpg_college.FloatRectangle;
 import foxie.rpg_college.Vec2;
 
+// Sama seperti Keyboard, Mouse melacak posisi
+// cursor sama juga kondisi tombolnya
 public class Mouse implements AutoCloseable {
   public enum Button {
     Left,
@@ -15,12 +17,21 @@ public class Mouse implements AutoCloseable {
     Right
   }
 
+  // berikut adalah data-data yang dibagi dengan listener
+  // beserta "lock object" yang digunakan untuk mensinkron
+  // modifikasi dan akses ke data berikut sehingga tidak
+  // dapat diakses bersamaan dengan modifikasi.
+  // NOTE: diperlukan karena listener berjalan di thread
+  // berbeda pada Java AWT
+  // ------------------------------------------------------
   // Shared with listener
   private final Object lock = new Object();
   private boolean buttonStateNow[] = {false, false, false};
   private boolean buttonStateClickedNow[] = {false, false, false};
   private Vec2 positionNow = new Vec2(0.0f, 0.0f);
 
+  // Data bawah ini melacak keadaan mouse sejak terakhir di
+  // update
   private boolean buttonStatePrev[] = {false, false, false};
   private State buttonState[] = {State.Unhold, State.Unhold, State.Unhold};
   private Vec2 positionSaved = new Vec2(0.0f, 0.0f);
@@ -46,11 +57,18 @@ public class Mouse implements AutoCloseable {
   }
 
   public void updateState() {
+    // Mengambil kunci di lock object
+    // agar listener tidak memodifikasi
+    // saat diakses
     synchronized (this.lock) {
+      // lalu logikanya sama dengan keyboard
+      // hanya tambahkan pemeriksaan this.buttonStateClickedNow
       for (int i = 0; i < this.buttonState.length; i++) {
         boolean prev = this.buttonStatePrev[i];
         boolean now = this.buttonStateNow[i];
         
+        // Periksa apakah tombol mouse diclick
+        // hasil langsung dari event MouseListener#mouseClicked
         if (this.buttonStateClickedNow[i]) {
           this.buttonState[i] = State.Clicked;
           continue;
@@ -109,13 +127,25 @@ public class Mouse implements AutoCloseable {
         return;
       }
       
+      // Mengambil koordinat utuhnya
       Vec2 raw = new Vec2((float) e.getX(), (float) e.getY());
+      
+      // Lalu mengoffset koordinat sehingga koordinat relatif
+      // dengan area yang di lihat oleh kelas ini
       Vec2 offseted = raw.sub(this.owner.watchedArea.getTopLeftCorner());
+      // Setelah itu hitung berapa banyak mouse bergerak dalam area
+      // watchedArea
       Vec2 percentMoved = offseted.div(this.owner.watchedArea.getSize());
       
+      // Dan akhirnya megubah persen tersebut sehingga menjadi koordinat
+      // dalam mapWatchedArea
+      // Hal ini adalah salah satu komponen yang penting untuk program agar
+      // ukuran window nya dapat resize sedangkan game hanya memikirkan area
+      // yang dia ingin lihat
       this.owner.positionNow = percentMoved.mul(this.owner.mapWatchedArea);
     }
 
+    // Mouse nya keluar dari window reset semuanya
     @Override
     public void mouseExited(MouseEvent e) {
       synchronized (this.owner.lock) {
@@ -158,6 +188,16 @@ public class Mouse implements AutoCloseable {
       }
     }
     
+    // Sisa dari method-methodnya hanya mengambil kuncil
+    // lalu memanggil method yang sesuai dengan argument
+    // yang sesuai. dan juga memeriksa apakah eventnya
+    // menarik atau tidak (terletak dalam window yang mau diperiksa)
+    //
+    // mousePressed untuk menekan tombol,
+    // mouseReleased untuk melepaskan tombol,
+    // mouseDrag untuk mouse yang bergerak tetapi juga menahan tombol
+    // mouseMoved untuk mouse yang bergerak tetapi tidak menahan tombol
+    // dan mouseEntered ignore saja, karena tidak penting
     @Override
     public void mousePressed(MouseEvent e) {
       synchronized (this.owner.lock) {
