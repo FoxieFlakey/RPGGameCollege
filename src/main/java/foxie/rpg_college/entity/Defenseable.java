2929688ef7d7ce1a16c8b4179e0ae764622d2b51
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 10:18:44 +0700

    Document Defenseable.java

--- src/main/java/foxie/rpg_college/entity/Defenseable.java
+++ src/main/java/foxie/rpg_college/entity/Defenseable.java
@@ -2,7 +2,24 @@ package foxie.rpg_college.entity;
 
 import foxie.rpg_college.entity.damage.DamageSource;
 
+// Interface ini adalah untuk entity/karakter
+// yang dapat bertahan dengan cara apapun
 public interface Defenseable {
+  // Mereturn true jika dapat mengblok
+  // atau bertahan. Jika false, tidak
+  // dapat. Contohnya return false karena
+  // mungkin mana terlalu rendah atau ada
+  // cooldown/limit waktu
   boolean canDefense();
+  
+  // Karakter/entity bertahan terhadap
+  // sebuah sumber damage method ini
+  // bebas mengeditnya seperti beberapa
+  // karakter menerima damage lebih rendah
+  // atau 100% memblok
+  //
+  // Setelah ini damagenya akan diapply.
+  // Method ini hanya dipanggil jika
+  // canDefense returnnya true
   void defend(DamageSource source);
 }
