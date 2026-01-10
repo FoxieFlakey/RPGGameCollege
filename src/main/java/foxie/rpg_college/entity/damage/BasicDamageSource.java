3a63e8489dede07837ae538b7fb514b9201e35da
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 10:54:49 +0700

    Document BasicDamageSource

--- src/main/java/foxie/rpg_college/entity/damage/BasicDamageSource.java
+++ src/main/java/foxie/rpg_college/entity/damage/BasicDamageSource.java
@@ -1,5 +1,10 @@
 package foxie.rpg_college.entity.damage;
 
+// Subclass dari DamageSource yang menyatakan
+// damage tetapi tidak tentu asal usulnya
+//
+// Kelas ini basic saja, sesuai namanya memungkinkan
+// pengguna menset damage sama nama, itu saja
 public class BasicDamageSource extends DamageSource {
   private final String name;
   
