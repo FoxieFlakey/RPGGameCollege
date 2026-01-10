d54d2cf854977d7c84f9f6156cabfea405a26bc8
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 10:57:24 +0700

    Document EntityDamageSource.java

--- src/main/java/foxie/rpg_college/entity/damage/EntityDamageSource.java
+++ src/main/java/foxie/rpg_college/entity/damage/EntityDamageSource.java
@@ -2,6 +2,11 @@ package foxie.rpg_college.entity.damage;
 
 import foxie.rpg_college.entity.Entity;
 
+// Bentuk damage lain yang berasal dari sebuah entity lain
+// contohnya seperti damage dari archer/mage/etc ataupun dari
+// turret atau karakter apa saja asal ada Entity nya yang
+// menjadi source. Jika tanpa source, pengguna seharusnya
+// pakai BasicDamageSource kelas bukan kelas ini
 public class EntityDamageSource extends DamageSource {
   private final Entity source;
   
@@ -14,6 +19,9 @@ public class EntityDamageSource extends DamageSource {
     return this.source;
   }
   
+  // Nama dari entity yang menyerang dapat
+  // diambil melalui method yang tersedia di
+  // semua entity
   @Override
   public String getName() {
     return this.source.getName();
