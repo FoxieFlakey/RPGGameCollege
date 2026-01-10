3489adaf294467461cbd2b4b1a19a4bff7191949
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 15:20:21 +0700

    Document TextureManager

--- src/main/java/foxie/rpg_college/texture/TextureManager.java
+++ src/main/java/foxie/rpg_college/texture/TextureManager.java
@@ -10,11 +10,17 @@ import javax.imageio.ImageIO;
 
 import foxie.rpg_college.Util;
 
+// Kelas texture ini bertugas untuk memanajemen
+// texture-texture yang game punya
 public class TextureManager {
   private final HashMap<String, Texture> textures = new HashMap<>();
   private final HashMap<String, Supplier<Texture>> loaders = new HashMap<>();
   private boolean isAllLoaded = false;
   
+  // Fungsi ini menambahkan tekstur ke map 'loaders'
+  // agar nanti texturenya di load dan dapat diakses
+  // lagi dengan 'getTexture' setelah diload meggunakan
+  // 'id' yang diberikan
   public void addTexture(String id, Supplier<Texture> loader) {
     if (this.isAllLoaded) {
       throw new IllegalStateException("Cannot add texture on manager loaded");
@@ -27,6 +33,8 @@ public class TextureManager {
     this.loaders.put(id, loader);
   }
   
+  // Sama seperti diatas, tetapi ini memudahkan kalau ingin
+  // menambah tekstur yang di load meggunakan fungsi Util.getResource
   public void addTexture(String id, String resourcePath) {
     this.addTexture(id, () -> {
       try {
@@ -37,6 +45,7 @@ public class TextureManager {
     });
   }
   
+  // Mencari texture yang memiliki id specific
   public Texture getTexture(String id) {
     if (!this.isAllLoaded) {
       throw new IllegalStateException("Textures not loaded yet");
@@ -49,6 +58,8 @@ public class TextureManager {
     return this.textures.get(id);
   }
   
+  // Mulai process untuk menload semua tekstur yang sudah
+  // ditambah. Setelah ini penambahkan tekstur baru tidak diizinkan
   public void loadAll() {
     if (this.isAllLoaded) {
       throw new IllegalStateException("Cannot load again");
