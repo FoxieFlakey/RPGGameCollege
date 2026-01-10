eac34dfe42545e20311a88bdfa72a1587a7957f9
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 10:24:50 +0700

    Document DamageSource

--- src/main/java/foxie/rpg_college/entity/damage/DamageSource.java
+++ src/main/java/foxie/rpg_college/entity/damage/DamageSource.java
@@ -1,5 +1,16 @@
 package foxie.rpg_college.entity.damage;
 
+// Kelas abstrak damage source, menjelaskan asal usul
+// sebuah damage. Kelas dasar ini isinya hanya menentukan
+// damage berapa. Subclassnya dapat menambahkan informasi
+// tambahkan seperti EntityDamageSource, menambahkan
+// damage yang berasal dari entity spesifik. Sedangkan
+// TileDamageSource menjelaskan damagenya berasal dari
+// tile contohnya lava. Ini memungkinkan kode-kode seperti
+// implementasi Defenseable memeriksa apakah karakter
+// dapat bertahan dari contohnya hanya dapat bertahan dari
+// arrows. Tetapi 100% gagal bertahan terhadap fireball
+// dan sumber damage lain
 public abstract class DamageSource {
   private float damagePoint;
   
@@ -11,6 +22,8 @@ public abstract class DamageSource {
     this.damagePoint = Math.max(0.0f, newDamage);
   }
   
+  // Method ini memeriksa apakah damage sourcenya
+  // 100% dicancel atau damage nya adalah 0.0f
   public boolean isCanceled() {
     return Math.signum(this.getDamagePoint()) < 1.0f && Math.signum(this.getDamagePoint()) > -1.0f;
   }
@@ -19,6 +32,10 @@ public abstract class DamageSource {
     return this.damagePoint;
   }
   
+  // Mereturn string yang mendeskripsikan
+  // sumber dari damagenya dapat digunakan
+  // dalam UI death screen. Untuk menulis
+  // alasan kematian
   public abstract String getName();
 }
 
