f0c2a03879988a4d777df9c13dcd590197cb26ed
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 15:16:08 +0700

    Document Texture.java

--- src/main/java/foxie/rpg_college/texture/Texture.java
+++ src/main/java/foxie/rpg_college/texture/Texture.java
@@ -2,6 +2,9 @@ package foxie.rpg_college.texture;
 
 import java.awt.image.BufferedImage;
 
+// Record ini menyimpan data berkaitan dengan
+// tekstur atau gambar beserta lebar and panjang
+// tidak banyak yang dilakukan untuk sekarang ini
 public record Texture(
   int width,
   int height,
@@ -11,6 +14,10 @@ public record Texture(
     this(Texture.validateSize(image.getWidth(null)), Texture.validateSize(image.getHeight(null)), image);
   }
   
+  // Fungsi ini diperlukan karena java.awt.Image menentukan
+  // kalau getWidth dan getHeight mungkin return hasil negatif
+  // jadi kalau itu terjadi lempar exception saja dari pada
+  // angka negatif tersebut menjadi tersembunyi
   private static int validateSize(int x) {
     if (x < 1.0) {
       throw new RuntimeException("Unexpected size");
