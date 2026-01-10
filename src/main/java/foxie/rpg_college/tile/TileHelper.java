ebbaaf5a60e47cffc6254a9a596e9a4914b785f0
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 14:29:25 +0700

    Document TileHelper.java

--- src/main/java/foxie/rpg_college/tile/TileHelper.java
+++ src/main/java/foxie/rpg_college/tile/TileHelper.java
@@ -6,6 +6,9 @@ import foxie.rpg_college.IVec2;
 import foxie.rpg_college.Vec2;
 
 public class TileHelper {
+  // Fungsi pembantu ini menghitung area kotak
+  // yang menjadi posisi Tilenya yang akan menjadi
+  // hasil rendernya di posisi layar
   public static FloatRectangle calculateRenderBox(Tile tile, IVec2 pos) {
     Camera camera = tile.getGame().getCamera();
     Vec2 fpos = Tile.fromTileCoordToWorldCoord(pos);
