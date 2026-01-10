6c2abe553a26f0b9f20e37ac0bd87e054a9fb971
Foxie Flakey <foxieflakey@gmail.com>
2026-01-10 13:21:05 +0700

    Dokumen State.java

--- src/main/java/foxie/rpg_college/input/State.java
+++ src/main/java/foxie/rpg_college/input/State.java
@@ -1,11 +1,19 @@
 package foxie.rpg_college.input;
 
+// enum yang menyatakan kondisi sebuah button
+// seperti keyboard atatu mouse
 public enum State {
+  // Sebelumnya tombol belum diklik tetapi sekarang diklik
   Clicked,
+  // Sebelumnya tombol diklik tetapi sekarang tidak diklik
   Unclicked,
+  // Sebelumnya tombol diklik dan sekarang juga masih diklik
   Hold,
+  // Sebelumnya tombol tidak diklik dan sekarang juga masih tidak diklik
   Unhold;
 
+  // Apakah state nya berakhir dengan
+  // tombol ditekan
   public boolean isNowPressed() {
     switch (this) {
       case Clicked:
@@ -17,6 +25,8 @@ public enum State {
     }
   }
 
+  // Apakah state nya berakhir dengan
+  // tombol tidak ditekan
   public boolean isNowReleased() {
     switch (this) {
       case Unclicked:
