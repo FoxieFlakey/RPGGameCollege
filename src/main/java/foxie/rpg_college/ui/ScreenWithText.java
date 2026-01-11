c3cf68a742eda9c7bbec364b29b0513fb9f674c7
Foxie Flakey <foxieflakey@gmail.com>
2026-01-11 15:22:10 +0700

    Document ScreenWithText

--- src/main/java/foxie/rpg_college/ui/ScreenWithText.java
+++ src/main/java/foxie/rpg_college/ui/ScreenWithText.java
@@ -7,6 +7,11 @@ import java.util.Iterator;
 import foxie.rpg_college.Game;
 import foxie.rpg_college.Vec2;
 
+// Subclass dari Screen yang menghandle menampilkan
+// teks dilayar dan method lain dibiarkan. Subclass dair
+// ini ada HelpScreen dan DeathScreen yang dua-duanya
+// ingin menampilkan teks tetapi tidak ingin menduplikatkan
+// kode
 public abstract class ScreenWithText extends Screen {
   public ScreenWithText(Game game) {
     super(game);
@@ -14,13 +19,19 @@ public abstract class ScreenWithText extends Screen {
 
   @Override
   public void render(Graphics2D g, float deltaTime) {
+    // Mengambil skala gamenya yaitu sebesar apa teks dirender
+    // relatif dengan output sebenarnya karena koordinat yang
+    // dipakai statis di 1280.0f x 720.0f jadi jika output lebih
+    // besar maka skala tentunya lebih besar
     float renderScale = this.getGame().getCamera().getScale().x();
     float width = this.getGame().getOutputWidth();
     float height = this.getGame().getOutputHeight();
     
+    // Menghitung dimana teks mulai dan berakhir
     Vec2 textStart = new Vec2(width * 0.10f, height * 0.10f);
     Vec2 textEnd = new Vec2(width * 0.90f, height * 0.90f);
     
+    // Pertama merender background
     g.setColor(new Color(0.0f, 0.0f, 0.0f, 0.60f));
     g.fillRect(
       (int) textStart.x(),
@@ -29,15 +40,23 @@ public abstract class ScreenWithText extends Screen {
       (int) (textEnd.y() - textStart.y())
     );
     
+    // Setelah itu merender teks
     g.setColor(Color.WHITE);
     g.setFont(Fonts.getDefault().deriveFont(30.0f * renderScale));
     
+    // Karena method drawString pada java.awt.Graphics2D tidak
+    // memeriksa newline, maka kita perlu manual
     Iterator<String> lines = this.getText().lines().iterator();
     int lineIndex = 0;
+
+    // Hitung tinggi baris
     int lineHeight = Fonts.getFontHeight(g);
     
+    // Lalu mengiterasi tiap baris dengan iterator
     while (lines.hasNext()) {
       String line = lines.next();
+
+      // Setelah itu gambar teks nya
       g.drawString(
         line,
         textStart.x() + (10.0f * renderScale),
@@ -48,5 +67,7 @@ public abstract class ScreenWithText extends Screen {
     }
   }
   
+  // Definisi method abstrak getText untuk mengambil
+  // teks apa yang akan ditampilkan
   public abstract String getText();
 }
