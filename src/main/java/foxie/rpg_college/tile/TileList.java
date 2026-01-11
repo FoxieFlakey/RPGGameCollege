f824f189829a58660bb2f21aa447e2b56e076f96
Foxie Flakey <foxieflakey@gmail.com>
2026-01-11 14:39:03 +0700

    Document TileList.java

--- src/main/java/foxie/rpg_college/tile/TileList.java
+++ src/main/java/foxie/rpg_college/tile/TileList.java
@@ -3,9 +3,21 @@ package foxie.rpg_college.tile;
 import foxie.rpg_college.Game;
 import foxie.rpg_college.WorldManager;
 
+// Menyimpan list-list tile yang ada dalam game
 public class TileList {
+  // Java memilki keyword 'final' yang berguna untuk konstanta
+  // 'final' memblokir perubahan pada sebuah method/class/variabel
+  // sehingga tidak berubah. Disini final dipakai ke variabel
+  // berarti tidak bisa diedit sama sekali oleh apapun. Karena
+  // final tersebut field lebih aman diexpose ke luar dan tidak
+  // ada kode yang mengubah isinnya.
+  
   public final WallTile WALL_TILE;
   public final LavaTile LAVA_TILE;
+
+  // Ini salah satu dua tile instance nya yang beda
+  // PortalTile memiliki tujuan yang berbeda jadi instance
+  // nya berbada.
   public final PortalTile PORTAL_TO_BATTLE;
   public final PortalTile PORTAL_TO_OVERWORLD;
 
