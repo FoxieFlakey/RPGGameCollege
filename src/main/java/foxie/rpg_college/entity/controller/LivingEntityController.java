6e329bdeb5864b8b60490d0370ef848eb3c07b27
Foxie Flakey <foxieflakey@gmail.com>
2026-01-11 20:24:45 +0700

    Document LivingEntityController

--- src/main/java/foxie/rpg_college/entity/controller/LivingEntityController.java
+++ src/main/java/foxie/rpg_college/entity/controller/LivingEntityController.java
@@ -2,11 +2,15 @@ package foxie.rpg_college.entity.controller;
 
 import foxie.rpg_college.entity.LivingEntity;
 
+// Sebuah subclass dari entity controller untuk
+// megisi beberapa method dengan data dari LivingEntity
 public class LivingEntityController extends EntityController {
   public LivingEntityController(LivingEntity owner) {
 		super(owner);
 	}
   
+  // Sama seperti di ProjectileEntiyController#getProjectileEntity
+  // ini memudahkan mengambil instance sebagai LivingEntity
   public LivingEntity getLivingEntity() {
     return (LivingEntity) this.getEntity();
   }
@@ -18,6 +22,8 @@ public class LivingEntityController extends EntityController {
   
   @Override
   public boolean shouldControlDisabled() {
+    // LivingEntity tidak dapat dikontrol kalau sudah
+    // mati
     return this.getLivingEntity().isDead();
   }
 }
