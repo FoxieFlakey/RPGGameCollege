7875ebd31b1865c7a8bf31da3f538b68de48b174
Foxie Flakey <foxieflakey@gmail.com>
2026-01-11 20:00:58 +0700

    Document ControlEventListener.java

--- src/main/java/foxie/rpg_college/entity/controller/ControlEventListener.java
+++ src/main/java/foxie/rpg_college/entity/controller/ControlEventListener.java
@@ -1,5 +1,15 @@
 package foxie.rpg_college.entity.controller;
 
+// Interface ini menjelaskan event-event yang
+// controller dapat dengar yang mungkin penting
+// seperti perubahan lokasi, ganti dunia atau
+// juga entity tidak dapat di kendalikan lagi
+//
+// Ini digunakan untuk kelas InputToControllerBridge
+// untuk mengupdate posisi kamera sama ukuran dunia
+//
+// Sedangkan onEntityNoLongerControllable untuk 
+// berhenti mengontrol entity :3
 public interface ControlEventListener {
   void onPositionUpdated();
   void onWorldChange();
