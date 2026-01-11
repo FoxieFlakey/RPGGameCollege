1c92a07de0c164372c550b5f93e2cfd46fff0deb
Foxie Flakey <foxieflakey@gmail.com>
2026-01-11 14:29:25 +0700

    Document InGame.java

--- src/main/java/foxie/rpg_college/ui/InGame.java
+++ src/main/java/foxie/rpg_college/ui/InGame.java
@@ -9,14 +9,37 @@ import foxie.rpg_college.Vec2;
 import foxie.rpg_college.entity.Entity;
 import foxie.rpg_college.texture.Texture;
 
+// Screen ini menampilkan berbagai HUD untuk menampilkan
+// berbagai informasi ke player saat dalam game. Awalnya
+// 
+// HUD juga menampilkan health, mana player tetapi saya
+// hapus karena kodenya berserakan sama redundan karena
+// tiap LivingEntity sama CharacterEntity menampilkan
+// mana dan HP diatas entity nya.
+//
+// Fungsi yang ada hanya menampilkan "selected entity" yang
+// menunjukkan entity yang player sedang kendalikan
 public class InGame extends Screen {
+  // Berapa lama crosshair akan mengadjust posisinya
+  // untuk sebuah animasi kecil saat entity yang
+  // dikendalikan berganti
+  //
+  // Animasi dapat dilihat saat game baru mulai atau
+  // respawn setelah mati.
   private static final float BEAT_TIME = 0.2f;
   
+  // Empat gambar untuk tiap-tiap sudut "selected entity"
+  // dari kiri atas, sampai kana bawah
   private final Texture topLeftCorner;
   private final Texture topRightCorner;
   private final Texture bottomRightCorner;
   private final Texture bottomLeftCorner;
   
+  // Menyimpan entity yang sedang di kendalikan
+  // 
+  // Optional juga sebuah kelas yang Java memiliki
+  // yang memudahkan pemeriksaan apakah objek ada
+  // atau tidak dibanding memakai value null.
   private Optional<Entity> currentEntity = Optional.empty();
   private float beatTimeLeft = 0.0f;
   
@@ -30,9 +53,15 @@ public class InGame extends Screen {
 
   @Override
   public boolean handleInput() {
+    // HUD game tidak memiliki button/atau apapun
+    // yang memeriksa input, jadi lanjutkan ke
+    // input yang menghandle game
     return true;
   }
 
+  // Tick kodenya adalah memajukan animasi untuk beat
+  // dan memulai animasi baru jika entity yang dikendalikan
+  // berganti
   @Override
   public void tick(float deltaTime) {
     Optional<Entity> maybeEntity = this.getGame().getPlayer();
@@ -54,10 +83,15 @@ public class InGame extends Screen {
     float offsetMultiplier = this.beatTimeLeft / InGame.BEAT_TIME;
     
     if (maybeEntity.isEmpty()) {
+      // Entity tidak ada, jadi jangan dirender
       return;
     }
     
     Entity e = maybeEntity.get();
+
+    // Mengambil kotak yang berisi entity nya
+    // atau hardcode ukuran 100x100 karena
+    // tidak ada kotak berisi entity nya
     FloatRectangle renderBox = e.getRenderBoundInWorld().orElseGet(() -> {
       return new FloatRectangle(
         e.getPos().add(new Vec2(-100.0f)),
@@ -65,6 +99,8 @@ public class InGame extends Screen {
       );
     });
     
+    // Setelah itu menghitung koordinat untuk
+    // tiap sudut lalu dirender
     Vec2 cornerSize = new Vec2(20.0f);
     Vec2 topLeft = renderBox.getTopLeftCorner()
       .sub(new Vec2(5.0f))
@@ -84,6 +120,8 @@ public class InGame extends Screen {
   }
   
   void drawCorner(Graphics2D g, Vec2 pos, Vec2 size, Texture cornerTexture) {
+    // Mentranslate posisi dan ukuran corner
+    // lalu langsung dipakai render gambar untuk sebuah sudut
     FloatRectangle renderBox = this.getGame().getCamera().translateWorldToAWTGraphicsCoord(pos, size);
     
     int x = (int) renderBox.getTopLeftCorner().x();
@@ -102,6 +140,8 @@ public class InGame extends Screen {
   
   @Override
   public boolean canTickGame() {
+    // karena InGame adalah HUD yang menampilkan keadaan
+    // game saat berjalan maka return true selalu
     return true;
   }
 }
