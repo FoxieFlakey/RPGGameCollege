2b3e4d20d7f3054cb6c37d7fca9c6ab55fde140b
Foxie Flakey <foxieflakey@gmail.com>
2026-01-11 15:27:17 +0700

    Document HelpScreen

--- src/main/java/foxie/rpg_college/ui/HelpScreen.java
+++ src/main/java/foxie/rpg_college/ui/HelpScreen.java
@@ -7,10 +7,18 @@ import foxie.rpg_college.input.Keyboard;
 import foxie.rpg_college.input.State;
 import foxie.rpg_college.input.Keyboard.Button;
 
+// Kelas ini menampilkan layar pembantu ke pengguna
 public class HelpScreen extends ScreenWithText {
+  // Menyimpan layar sebelumnya sehingga
+  // saat keluar dari help screen mereturn
+  // ke screen sebelumnya
   private final Screen prevScreen;
   
+  // Halaman yang sedang aktif, basis 0
+  // seperti Java perlukan
   private int currentPage = 0;
+
+  // Konstanta, dari teks untuk layar pembantu
   private static final String[] PAGES = {
       ////////////////////////////////////////
       """
@@ -56,6 +64,10 @@ public class HelpScreen extends ScreenWithText {
       ////////////////////////////////////////
     };
   
+  // HelpScreen memerlukan reference ke layar
+  // sebelumnya agar dapat direturn sama dirender
+  // layar sebelumnya. Karena helpscreen menimpa
+  // output dari layar sebelumnya
   public HelpScreen(Game game, Screen prevScreen) {
     super(game);
     this.prevScreen = prevScreen;
@@ -63,6 +75,8 @@ public class HelpScreen extends ScreenWithText {
 
   @Override
   public String getText() {
+    // Menampilkan teks yang sedang aktif
+    // beserta teks nya
     return
       "Page " + (this.currentPage + 1) + "/" + HelpScreen.PAGES.length + ". Esc to quit.\n\n" +
       HelpScreen.PAGES[this.currentPage];
@@ -70,13 +84,21 @@ public class HelpScreen extends ScreenWithText {
 
   @Override
   public boolean handleInput() {
+    // Menghandle input untuk help screennya
+
     Keyboard keyboard = this.getGame().getKeyboard();
     if (keyboard.getState(Button.Escape) == State.Clicked) {
+      // Escape untuk keluar lalu set layar menjadi layar
+      // sebelumnya lalu return false, agar input tidak
+      // dilanjutkan ke kode berikutnya
       this.getGame().setScreen(this.prevScreen);
       return false;
     }
     
     if (keyboard.getState(Button.ArrowLeft) == State.Clicked) {
+      // bergerak ke kiri, halaman -1 lalu mencek
+      // agar halamat tetap berhenti di 0, menghindari
+      // menghindari out of bound pada array
       this.currentPage -= 1;
       if (this.currentPage < 0) {
         this.currentPage = 0;
@@ -84,27 +106,37 @@ public class HelpScreen extends ScreenWithText {
     }
     
     if (keyboard.getState(Button.ArrowRight) == State.Clicked) {
+      // bergerak ke kanan, halaman +1 lalu mencek
+      // agar halamat tetap berhenti di index maximum,
+      // menghindari out of bound pada array
       this.currentPage += 1;
       if (this.currentPage >= HelpScreen.PAGES.length) {
         this.currentPage = HelpScreen.PAGES.length - 1;
       }
     }
     
+    // Return false karena semua input dicuri oleh
+    // layar ini hehe UwU
     return false;
   }
 
   @Override
   public boolean canTickGame() {
+    // Saat help screen muncul game dipause
     return false;
   }
   
   @Override
   public void tick(float deltaTime) {
+    // Layar pembantu tidak melakukan apa-apa
   }
   
   @Override
   public void render(Graphics2D g, float deltaTime) {
+    // Petama render layar sebelumnya
     this.prevScreen.render(g, deltaTime);
+
+    // Lalu timpa dengan output layar ini
     super.render(g, deltaTime);
   }
 }
