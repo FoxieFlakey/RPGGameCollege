7065cedf429ec93c7570d646e80f83fb18cac39b
Foxie Flakey <foxieflakey@gmail.com>
2026-01-12 12:34:48 +0700

    Document HeroCharacter.java

--- src/main/java/foxie/rpg_college/entity/HeroCharacter.java
+++ src/main/java/foxie/rpg_college/entity/HeroCharacter.java
@@ -2,6 +2,9 @@ package foxie.rpg_college.entity;
 
 import foxie.rpg_college.Game;
 
+// Kelas hero, salah satu kelas diminta untuk assignment tugas college
+// kelas ini tidak ada fungsi special selain memenuhi kebutuhan tugasnya
+// AAA ;w;
 public abstract class HeroCharacter extends CharacterEntity {
   public HeroCharacter(Game game) {
     super(game);
