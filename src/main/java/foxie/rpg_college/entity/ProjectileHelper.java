f4257cd0cfe1dc65d3897060c6f12eeb13acf3ff
Foxie Flakey <foxieflakey@gmail.com>
2026-01-12 11:32:43 +0700

    Document ProjectileHelper

--- src/main/java/foxie/rpg_college/entity/ProjectileHelper.java
+++ src/main/java/foxie/rpg_college/entity/ProjectileHelper.java
@@ -7,6 +7,10 @@ import java.awt.geom.AffineTransform;
 import foxie.rpg_college.Vec2;
 
 public class ProjectileHelper {
+  // Method ini menyederhanakan kode yang meggambar projectile
+  // render ini juga menggerakkan gambarnya agak kebelakang sehingga hitboxnya
+  // sebenarnya di ujung projectile rata-rata seperti arrow yang kena kan
+  // ujung panahnya contohnya :3
   public static void renderProjectile(Entity e, Graphics2D g, Image texture, Vec2 renderSize) {
     float textureWidth = texture.getWidth(null);
     float texureHeight = texture.getHeight(null);
